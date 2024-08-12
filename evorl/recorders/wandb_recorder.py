9cf55a9a1e519af068f982e75b1d27ad89b5af63
zaber <20830726+ZaberKo@users.noreply.github.com>
2024-08-12 13:25:40 +0800

    update WandbRecorder

--- evorl/recorders/wandb_recorder.py
+++ evorl/recorders/wandb_recorder.py
@@ -1,6 +1,5 @@
 from collections.abc import Mapping
 from typing import Any
-
 import wandb
 
 from .recorder import Recorder
@@ -10,24 +9,19 @@ class WandbRecorder(Recorder):
     def __init__(
         self, *, project, name, config, tags, path, mode="disabled", **wandb_kwargs
     ):
-        self.project = project
-        self.name = name
-        self.config = config
-        self.tags = tags
-        self.dir = path
-        self.mode = mode
-        self.wandb_kwargs = wandb_kwargs
+        self.wandb_kwargs = {
+            "project": project,
+            "name": name,
+            "config": config,
+            "tags": tags,
+            "dir": path,
+            "mode": mode,
+            "settings": wandb.Settings(start_method="thread"),
+            **wandb_kwargs,
+        }
 
     def init(self) -> None:
-        wandb.init(
-            project=self.project,
-            name=self.name,
-            config=self.config,
-            tags=self.tags,
-            dir=self.dir,
-            mode=self.mode,
-            **self.wandb_kwargs,
-        )
+        wandb.init(**self.wandb_kwargs)
 
     def write(self, data: Mapping[str, Any], step: int | None = None) -> None:
         wandb.log(data, step=step)
