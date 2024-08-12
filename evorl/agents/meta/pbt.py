4622f1bed14eb38751f96fe7bbe4f303c2513476
zaber <20830726+ZaberKo@users.noreply.github.com>
2024-08-12 22:10:48 +0800

    unify pbt API & decouple exploit_and_explore

--- evorl/agents/meta/pbt.py
+++ evorl/agents/meta/pbt.py
@@ -1,4 +1,5 @@
 import copy
+import logging
 import math
 from functools import partial
 
@@ -8,19 +9,35 @@ import jax
 import jax.numpy as jnp
 import jax.tree_util as jtu
 import optax
-import orbax.checkpoint as ocp
-from omegaconf import DictConfig, OmegaConf, open_dict, read_write
 from optax.schedules import InjectStatefulHyperparamsState
+import orbax.checkpoint as ocp
 
-from evorl.metrics import MetricBase, TrainMetric
+from jax.sharding import Mesh, NamedSharding
+from jax.sharding import PartitionSpec as P
+
+from omegaconf import DictConfig, OmegaConf, open_dict, read_write
+
+from evorl.distributed import (
+    POP_AXIS_NAME,
+    tree_device_get,
+    tree_device_put,
+    parallel_map,
+)
+from evorl.metrics import MetricBase
 from evorl.types import PyTreeData, PyTreeDict, State
 from evorl.utils.jax_utils import tree_last
-from evorl.workflows import RLWorkflow
+from evorl.workflows import RLWorkflow, Workflow
+from evorl.metrics import WorkflowMetric
+
+from .pbt_operations import explore, select
+
+logger = logging.getLogger(__name__)
 
 
 class TrainMetric(MetricBase):
     pop_episode_returns: chex.Array
     pop_episode_lengths: chex.Array
+    pop_train_metrics: MetricBase
 
 
 class EvalMetric(MetricBase):
@@ -32,23 +49,55 @@ class HyperParams(PyTreeData):
     lr: chex.Array
 
 
-class PBTWorkflow(RLWorkflow):
+class PBTWorkflow(Workflow):
     def __init__(self, workflow: RLWorkflow, config: DictConfig):
         super().__init__(config)
 
         self.workflow = workflow
+        self.devices = jax.local_devices()[:1]
+        self.sharding = None
 
     @classmethod
-    def build_from_config(cls, config: DictConfig, enable_jit: bool = True):
+    def name(cls):
+        return "PBT"
+
+    @property
+    def enable_multi_devices(self) -> bool:
+        return self.sharding is not None
+
+    @staticmethod
+    def _rescale_config(config) -> None:
+        num_devices = jax.device_count()
+
+        if config.pop_size % num_devices != 0:
+            logger.warning(
+                f"pop_size({config.pop_size}) cannot be divided by num_devices({num_devices}), "
+                f"rescale pop_size to {config.pop_size // num_devices * num_devices}"
+            )
+
+        config.pop_size = (config.pop_size // num_devices) * num_devices
+
+    @classmethod
+    def build_from_config(
+        cls, config: DictConfig, enable_multi_devices=True, enable_jit: bool = True
+    ):
         config = copy.deepcopy(config)  # avoid in-place modification
 
-        # Tips: Multi-Device Training is done by sub-workflow
+        devices = jax.local_devices()
+
+        OmegaConf.set_readonly(config, False)
+        cls._rescale_config(config)
+
         if enable_jit:
             cls.enable_jit()
 
         OmegaConf.set_readonly(config, True)
 
         workflow = cls._build_from_config(config)
+
+        mesh = Mesh(devices, axis_names=(POP_AXIS_NAME,))
+        workflow.devices = devices
+        workflow.sharding = NamedSharding(mesh, P(POP_AXIS_NAME))
 
         return workflow
 
@@ -60,25 +109,19 @@ class PBTWorkflow(RLWorkflow):
 
         devices = jax.local_devices()
 
-        # OmegaConf.set_struct(target_workflow_config, True)
-
         with read_write(target_workflow_config):
             with open_dict(target_workflow_config):
                 target_workflow_config.env = copy.deepcopy(config.env)
+                # disable target workflow ckpt
                 target_workflow_config.checkpoint = OmegaConf.create(dict(enable=False))
 
         OmegaConf.set_readonly(target_workflow_config, True)
 
-        if len(devices) > 1:
-            target_workflow = target_workflow_cls.build_from_config(
-                target_workflow_config,
-                enable_multi_devices=True,
-                devices=devices,
-            )
-        else:
-            target_workflow = target_workflow_cls.build_from_config(
-                target_workflow_config, enable_jit=True
-            )
+        target_workflow = target_workflow_cls.build_from_config(
+            target_workflow_config, enable_jit=True
+        )
+
+        target_workflow.devices = devices
 
         return cls(target_workflow, config)
 
@@ -90,10 +133,6 @@ class PBTWorkflow(RLWorkflow):
             optax.adam, static_args=("b1", "b2", "eps", "eps_root")
         )(learning_rate=self.config.search_space.lr.low)
 
-        pop_workflow_state = jax.vmap(self.workflow.setup)(
-            jax.random.split(workflow_key, pop_size)
-        )
-
         pop = PyTreeDict(
             lr=jax.random.uniform(
                 pop_key,
@@ -102,12 +141,25 @@ class PBTWorkflow(RLWorkflow):
                 maxval=self.config.search_space.lr.high,
             )
         )
+        pop = tree_device_put(pop, self.sharding)
 
-        pop_workflow_state = jax.vmap(apply_hyperparams_to_workflow_state)(
-            pop, pop_workflow_state
-        )
+        # save metric on GPU0
+        workflow_metrics = WorkflowMetric()
 
-        workflow_metrics = self._setup_workflow_metrics()
+        workflow_keys = jax.random.split(workflow_key, pop_size)
+        workflow_keys = jax.device_put(workflow_keys, self.sharding)
+
+        pop_workflow_state = jax.jit(
+            jax.vmap(self.workflow.setup, spmd_axis_name=POP_AXIS_NAME),
+            in_shardings=self.sharding,
+            out_shardings=self.sharding,
+        )(workflow_keys)
+
+        pop_workflow_state = jax.jit(
+            jax.vmap(apply_hyperparams_to_workflow_state, spmd_axis_name=POP_AXIS_NAME),
+            in_shardings=self.sharding,
+            out_shardings=self.sharding,
+        )(pop, pop_workflow_state)
 
         return State(
             key=key,
@@ -127,40 +179,44 @@ class PBTWorkflow(RLWorkflow):
                 return wf_state, train_metrics
 
             wf_state, train_metrics_trajectory = jax.lax.scan(
-                _one_step, wf_state, None, length=self.config.per_iter_workflow_steps
+                _one_step, wf_state, (), length=self.config.per_iter_workflow_steps
             )
+
+            # jax.debug.print("{x}", x=train_metrics_trajectory.train_episode_return)
 
             train_metrics = tree_last(train_metrics_trajectory)
 
             return train_metrics, wf_state
 
         if self.config.parallel_train:
-            pop_train_metrics, pop_workflow_state = jax.vmap(_train_steps)(
-                pop_workflow_state
-            )
+            train_steps_fn = jax.vmap(_train_steps, spmd_axis_name=POP_AXIS_NAME)
         else:
-            pop_train_metrics, pop_workflow_state = jax.lax.map(
-                _train_steps, pop_workflow_state
-            )
+            # TODO: fix potential unneccesary gpu-comm: eg: all-gather in ppo #line=387
+            # train_steps_fn = partial(jax.lax.map, _train_steps)
+            train_steps_fn = parallel_map(_train_steps, self.sharding)
+
+        pop_train_metrics, pop_workflow_state = jax.jit(
+            train_steps_fn, in_shardings=self.sharding, out_shardings=self.sharding
+        )(pop_workflow_state)
 
         # ===== eval ======
         if self.config.parallel_eval:
-            pop_eval_metrics, pop_workflow_state = jax.vmap(self.workflow.evaluate)(
-                pop_workflow_state
-            )
+            eval_fn = jax.vmap(self.workflow.evaluate, spmd_axis_name=POP_AXIS_NAME)
         else:
-            pop_eval_metrics, pop_workflow_state = jax.lax.map(
-                self.workflow.evaluate, pop_workflow_state
-            )
+            eval_fn = parallel_map(self.workflow.evaluate, self.sharding)
+
+        pop_eval_metrics, pop_workflow_state = jax.jit(
+            eval_fn, in_shardings=self.sharding, out_shardings=self.sharding
+        )(pop_workflow_state)
+
+        # customize your pop metrics here
         pop_episode_returns = pop_eval_metrics.episode_returns
 
         # ===== warmup or exploit & explore ======
         key, exploit_and_explore_key = jax.random.split(state.key)
 
-        def _dummy_fn(key, pop_episode_returns, pop, pop_workflow_state):
+        def _dummy_fn(pop, pop_workflow_state, pop_metrics, key):
             return pop, pop_workflow_state
-
-        _exploit_and_explore = partial(exploit_and_explore, self.config)
 
         pop, pop_workflow_state = jax.lax.cond(
             state.metrics.iterations + 1
@@ -168,11 +224,11 @@ class PBTWorkflow(RLWorkflow):
                 self.config.warmup_steps / self.config.per_iter_workflow_steps
             ),
             _dummy_fn,
-            _exploit_and_explore,
-            exploit_and_explore_key,
-            pop_episode_returns,
+            self.exploit_and_explore,
             pop,
             pop_workflow_state,
+            pop_episode_returns,
+            exploit_and_explore_key,
         )
 
         # ===== record metrics ======
@@ -184,7 +240,10 @@ class PBTWorkflow(RLWorkflow):
         train_metrics = TrainMetric(
             pop_episode_returns=pop_eval_metrics.episode_returns,
             pop_episode_lengths=pop_eval_metrics.episode_lengths,
+            pop_train_metrics=pop_train_metrics,
         )
+
+        train_metrics = tree_device_get(train_metrics, self.devices[0])
 
         return train_metrics, state.replace(
             key=key,
@@ -207,14 +266,15 @@ class PBTWorkflow(RLWorkflow):
     def evaluate(self, state: State) -> State:
         # Tips: evaluation consumes every workflow_state's internal key
         if self.config.parallel_eval:
-            pop_eval_metrics, pop_workflow_state = jax.vmap(self.workflow.evaluate)(
-                pop_workflow_state
-            )
+            eval_fn = jax.vmap(self.workflow.evaluate, spmd_axis_name=POP_AXIS_NAME)
         else:
-            pop_eval_metrics, pop_workflow_state = jax.lax.map(
-                self.workflow.evaluate, pop_workflow_state
-            )
+            eval_fn = parallel_map(self.workflow.evaluate, self.sharding)
 
+        pop_eval_metrics, pop_workflow_state = jax.jit(
+            eval_fn, in_shardings=self.sharding, out_shardings=self.sharding
+        )(pop_workflow_state)
+
+        # customize your pop metrics here
         eval_metrics = EvalMetric(
             pop_episode_returns=pop_eval_metrics.episode_returns,
             pop_episode_lengths=pop_eval_metrics.episode_lengths,
@@ -222,54 +282,61 @@ class PBTWorkflow(RLWorkflow):
 
         return eval_metrics, state.replace(pop_workflow_state=pop_workflow_state)
 
+    def exploit_and_explore(
+        self,
+        pop: chex.ArrayTree,
+        pop_workflow_state: State,
+        pop_metrics: chex.Array,
+        key: chex.PRNGKey,
+    ):
+        exploit_key, explore_key = jax.random.split(key)
 
-def exploit_and_explore(config, key, pop_episode_returns, pop, pop_workflow_state):
-    bottoms_num = round(config.pop_size * config.bottom_ratio)
-    tops_num = round(config.pop_size * config.top_ratio)
-    indices = jnp.argsort(pop_episode_returns)
-    bottoms_indices = indices[:bottoms_num]
-    tops_indices = indices[-tops_num:]
+        config = self.config
 
-    key, exploit_key, explore_key = jax.random.split(key, 3)
+        tops_indices, bottoms_indices = select(
+            pop_metrics,  # using episode_return
+            exploit_key,
+            bottoms_num=round(config.pop_size * config.bottom_ratio),
+            tops_num=round(config.pop_size * config.top_ratio),
+        )
 
-    def _exploit_and_explore_fn(key, top, top_wf_state):
-        new = PyTreeDict()
-        for hp_name in top.keys():
-            new[hp_name] = top[hp_name] * (
-                1
-                + jax.random.uniform(
-                    key,
-                    minval=-config.perturb_factor[hp_name],
-                    maxval=config.perturb_factor[hp_name],
-                )
+        parents = _pop_read(tops_indices, pop)
+        parents_wf_state = _pop_read(tops_indices, pop_workflow_state)
+
+        offsprings = jax.vmap(
+            partial(
+                explore,
+                perturb_factor=config.perturb_factor,
+                search_space=config.search_space,
             )
-        # TODO: check deepcopy is necessary (does not change the original state)
+        )(parents, jax.random.split(explore_key, bottoms_indices.shape[0]))
 
-        new_wf_state = apply_hyperparams_to_workflow_state(new, top_wf_state)
-        return new, new_wf_state
+        # Note: no need to deepcopy parents wf_state here
+        offsprings_workflow_state = jax.vmap(apply_hyperparams_to_workflow_state)(
+            offsprings, parents_wf_state
+        )
 
-    # replace bottoms with random tops
-    tops_choice_indices = jax.random.choice(
-        exploit_key, tops_indices, (len(bottoms_indices),)
-    )
+        # ==== survival | merge population ====
+        pop = _pop_write(bottoms_indices, pop, offsprings)
+        # we copy wf_state back to offspring wf_state
+        pop_workflow_state = _pop_write(
+            bottoms_indices, pop_workflow_state, offsprings_workflow_state
+        )
 
-    def _read(indices, pop):
-        return jtu.tree_map(lambda x: x[indices], pop)
+        return pop, pop_workflow_state
 
-    def _write(indices, pop, new):
-        return jtu.tree_map(lambda x, y: x.at[indices].set(y), pop, new)
+    @classmethod
+    def enable_jit(cls) -> None:
+        cls.evaluate = jax.jit(cls.evaluate, static_argnums=(0,))
+        cls.step = jax.jit(cls.step, static_argnums=(0,))
 
-    new_bottoms, new_bottoms_wf_state = jax.vmap(_exploit_and_explore_fn)(
-        jax.random.split(explore_key, len(bottoms_indices)),
-        _read(tops_choice_indices, pop),
-        _read(tops_choice_indices, pop_workflow_state),
-    )
-    pop = _write(bottoms_indices, pop, new_bottoms)
-    pop_workflow_state = _write(
-        bottoms_indices, pop_workflow_state, new_bottoms_wf_state
-    )
 
-    return pop, pop_workflow_state
+def _pop_read(indices, pop):
+    return jtu.tree_map(lambda x: x[indices], pop)
+
+
+def _pop_write(indices, pop, new):
+    return jtu.tree_map(lambda x, y: x.at[indices].set(y), pop, new)
 
 
 def apply_hyperparams_to_workflow_state(
--- evorl/distributed/sharding.py
+++ evorl/distributed/sharding.py
@@ -1,5 +1,7 @@
 import chex
 import jax
+from jax.experimental.shard_map import shard_map
+from collections.abc import Callable
 
 
 def tree_device_put(tree: chex.ArrayTree, device):
@@ -10,3 +12,22 @@ def tree_device_get(tree: chex.ArrayTree, device=None):
     if device is None:
         device = jax.devices()[0]
     return tree_device_put(tree, device)
+
+
+def parallel_map(fn: Callable, sharding):
+    """
+    sequential on the same gpu, parrallel on different gpu.
+    """
+
+    def shmap_f(state):
+        # state: sharded state on single device
+        # jax.debug.print("{}", state.env_state.obs.shape)
+        return jax.lax.map(fn, state)
+
+    return shard_map(
+        shmap_f,
+        mesh=sharding.mesh,
+        in_specs=sharding.spec,
+        out_specs=sharding.spec,
+        check_rep=False,
+    )
