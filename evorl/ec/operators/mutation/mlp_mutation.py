251a1aa32dbce3b344845a0f6e8bcf580cb2f2fc
zaber <20830726+ZaberKo@users.noreply.github.com>
2024-10-02 08:49:53 +0800

    add normal mutation

--- evorl/ec/operators/mutation/mlp_mutation.py
+++ evorl/ec/operators/mutation/mlp_mutation.py
@@ -14,13 +14,10 @@ def mlp_mutate(
     x: chex.ArrayTree,
     key: chex.PRNGKey,
     *,
-    weight_max_magnitude: float = 1e6,
-    mut_strength: float = 0.1,
-    num_mutation_frac: float = 0.1,
-    super_mut_strength: float = 10.0,
-    super_mut_prob: float = 0.05,
-    reset_prob: float = 0.05,
-    vec_relative_prob: float = 0.0,
+    weight_max_magnitude: float = 10,
+    mut_strength: float = 0.01,
+    vector_num_mutation_frac: float = 0.0,
+    matrix_num_mutation_frac: float = 0.01,
 ):
     """
     Args:
@@ -31,10 +28,19 @@ def mlp_mutate(
     """
 
     leaves, treedef = jtu.tree_flatten_with_path(x)
-    key, ssne_key = jax.random.split(key)
 
-    # prob thresould of whether mutate a param
-    ssne_probs = jax.random.uniform(ssne_key, (len(leaves),)) * 2
+    def _mutate(x, key, num_mutation_frac):
+        num_mutations = round(num_mutation_frac * param.size)
+        key, ind_key, normal_update_key = jax.random.split(key, 3)
+        # unlike ERL, we sample elements without replacement
+        flat_ind = jax.random.choice(
+            ind_key, param.size, (num_mutations,), replace=False
+        )
+        ind = jnp.unravel_index(flat_ind, param.shape)
+        updates = jax.random.normal(normal_update_key, (num_mutations,)) * mut_strength
+        param = param.at[ind].set(param[ind] + updates, unique_indices=True)
+        param = jnp.clip(param, -weight_max_magnitude, weight_max_magnitude)
+        return param
 
     params = []
     for i, (path, param) in enumerate(leaves):
@@ -43,89 +49,10 @@ def mlp_mutate(
             continue
 
         if param.ndim == 2:  # kernel
-            # Note: We use fixed number of mutations for a param,
-            # This is a little different from the original ERL
-            num_mutations = round(num_mutation_frac * param.size)
-
-            (
-                key,
-                ind_key,
-                prob_key,
-                normal_update_key,
-                reset_update_key,
-                ssne_prob_key,
-            ) = jax.random.split(key, 6)
-
-            # unlike ERL, we sample elements without replacement
-            num_param = param.shape[0] * param.shape[1]
-            flat_ind = jax.random.choice(
-                ind_key, num_param, (num_mutations,), replace=False
-            )
-            ind = jnp.unravel_index(flat_ind, param.shape)
-
-            prob = jax.random.uniform(prob_key, (num_mutations,))
-            super_mask = prob < super_mut_prob
-            reset_mask = jnp.logical_and(
-                prob >= super_mut_prob, prob < reset_prob + super_mut_prob
-            )
-
-            updates = jax.random.normal(normal_update_key, (num_mutations,)) * jnp.abs(
-                param[ind]
-            )
-            updates = jnp.where(
-                super_mask, updates * super_mut_strength, updates * mut_strength
-            )
-
-            reset_param = jax.random.normal(reset_update_key, (num_mutations,))
-            new_param = param.at[ind].set(
-                jnp.where(reset_mask, reset_param, param[ind] + updates),
-                unique_indices=True,
-            )
-
-            ssne_prob = jax.random.uniform(ssne_prob_key)
-            param = jnp.where(ssne_prob < ssne_probs[i], new_param, param)
-
-            param = jnp.clip(param, -weight_max_magnitude, weight_max_magnitude)
-
+            param = _mutate(param, key, matrix_num_mutation_frac)
         elif param.ndim == 1:  # bias or layer norm
-            if vec_relative_prob > 0:
-                num_mutations = round(num_mutation_frac * param.size)
-
-                (
-                    key,
-                    ind_key,
-                    prob_key,
-                    normal_update_key,
-                    reset_update_key,
-                    ssne_prob_key,
-                ) = jax.random.split(key, 6)
-
-                ind = jax.random.choice(ind_key, param.shape[0], (num_mutations,))
-
-                prob = jax.random.uniform(prob_key, (num_mutations,))
-                super_mask = prob < super_mut_prob
-                reset_mask = jnp.logical_and(prob >= super_mut_prob, prob < reset_prob)
-
-                updates = jax.random.normal(
-                    normal_update_key, (num_mutations,)
-                ) * jnp.abs(param[ind])
-                updates = jnp.where(
-                    super_mask, updates * super_mut_strength, updates * mut_strength
-                )
-
-                reset_param = jax.random.normal(reset_update_key, (num_mutations,))
-                new_param = param.at[ind].set(
-                    jnp.where(reset_mask, reset_param, param[ind] + updates),
-                    unique_indices=True,
-                )
-
-                ssne_prob = jax.random.uniform(ssne_prob_key)
-                param = jnp.where(
-                    ssne_prob < ssne_probs[i] * vec_relative_prob, new_param, param
-                )
-
-                param = jnp.clip(param, -weight_max_magnitude, weight_max_magnitude)
-
+            if vector_num_mutation_frac > 0:
+                param = _mutate(param, key, vector_num_mutation_frac)
         else:
             raise ValueError(f"Unsupported parameter shape: {param.shape}")
 
@@ -135,13 +62,10 @@ def mlp_mutate(
 
 
 class MLPMutation(PyTreeNode):
-    weight_max_magnitude: float = 1e6
-    mut_strength: float = 0.1
-    num_mutation_frac: float = 0.1
-    super_mut_strength: float = 10.0
-    super_mut_prob: float = 0.05
-    reset_prob: float = 0.05
-    vec_relative_prob: float = 0.0
+    weight_max_magnitude: float = 10
+    mut_strength: float = 0.01
+    vector_num_mutation_frac: float = 0.0
+    matrix_num_mutation_frac: float = 0.01
 
     mutate_fn: Callable = pytree_field(lazy_init=True, pytree_node=False)
 
@@ -155,11 +79,8 @@ class MLPMutation(PyTreeNode):
                 mlp_mutate,
                 weight_max_magnitude=self.weight_max_magnitude,
                 mut_strength=self.mut_strength,
-                num_mutation_frac=self.num_mutation_frac,
-                super_mut_strength=self.super_mut_strength,
-                super_mut_prob=self.super_mut_prob,
-                reset_prob=self.reset_prob,
-                vec_relative_prob=self.vec_relative_prob,
+                vector_num_mutation_frac=self.vector_num_mutation_frac,
+                matrix_num_mutation_frac=self.matrix_num_mutation_frac,
             ),
         )
 
--- evorl/ec/optimizers/erl_ga.py
+++ evorl/ec/optimizers/erl_ga.py
@@ -6,28 +6,31 @@ import jax.numpy as jnp
 import jax.tree_util as jtu
 
 from evorl.types import PyTreeData, pytree_field
-from evorl.ec.operators import MLPMutation, MLPCrossover, TournamentSelection
+from evorl.ec.operators import ERLMutation, MLPCrossover, TournamentSelection
 
 from .ec_optimizer import EvoOptimizer, ECState
 
 
-class GAState(PyTreeData):
+class ERLGAState(PyTreeData):
     pop: chex.ArrayTree
     key: chex.PRNGKey
 
 
-class GA(EvoOptimizer):
+class ERLGA(EvoOptimizer):
     pop_size: int
     num_elites: int
 
     # selection
-    tournament_size: int = 2
+    tournament_size: int = 3
 
     # mutation
     weight_max_magnitude: float = 1e6
     mut_strength: float = 0.1
-    vector_num_mutation_frac: float = 0.0
-    matrix_num_mutation_frac: float = 0.01
+    num_mutation_frac: float = 0.1
+    super_mut_strength: float = 10.0
+    super_mut_prob: float = 0.05
+    reset_prob: float = 0.1
+    vec_relative_prob: float = 0.0
 
     # crossover
     enable_crossover: bool = True
@@ -44,11 +47,14 @@ class GA(EvoOptimizer):
         ), "(pop_size - num_elites) must be even when enable crossover"
 
         selection_op = TournamentSelection(tournament_size=self.tournament_size)
-        mutation_op = MLPMutation(
+        mutation_op = ERLMutation(
             weight_max_magnitude=self.weight_max_magnitude,
             mut_strength=self.mut_strength,
-            vector_num_mutation_frac=self.vector_num_mutation_frac,
-            matrix_num_mutation_frac=self.matrix_num_mutation_frac,
+            num_mutation_frac=self.num_mutation_frac,
+            super_mut_strength=self.super_mut_strength,
+            super_mut_prob=self.super_mut_prob,
+            reset_prob=self.reset_prob,
+            vec_relative_prob=self.vec_relative_prob,
         )
         crossover_op = MLPCrossover(num_crossover_frac=self.num_crossover_frac)
 
@@ -57,7 +63,7 @@ class GA(EvoOptimizer):
         self.set_frozen_attr("crossover", crossover_op)
 
     def init(self, pop, key) -> ECState:
-        return GAState(pop=pop, key=key)
+        return ERLGAState(pop=pop, key=key)
 
     def tell(self, state: ECState, xs: chex.ArrayTree, fitnesses: chex.Array):
         # Note: We simplify the update in ERL
